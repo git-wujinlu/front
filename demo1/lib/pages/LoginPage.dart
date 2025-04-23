b0ba34201e2c6c14c2e1478401a9c621d0cf1ced
王畅 <15147214+sangetsi@user.noreply.gitee.com>
2025-04-23 21:33:44 +0800

    signup

--- demo1/lib/pages/LoginPage.dart
+++ demo1/lib/pages/LoginPage.dart
@@ -10,12 +10,45 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _obscurePassword = true;
+  final TextEditingController _passwordCheckController =
+      TextEditingController();
+  bool _obscurePasswordCheck = true;
+  bool passwordChecked = true;
+  int nowPage = 0;
+
+  void login() {
+    if (nowPage == 1) {
+      setState(() {
+        nowPage = 0;
+      });
+    } else {
+      Navigator.pushNamedAndRemoveUntil(context, '/homepage', (route) => false);
+    }
+  }
+
+  void signUp() {
+    if (nowPage == 0) {
+      setState(() {
+        nowPage = 1;
+      });
+    } else {
+      if (!passwordChecked) {
+      } else {
+        setState(() {
+          nowPage = 0;
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     final height = size.height;
     final width = size.width;
-    final TextEditingController _usernameController = TextEditingController();
     return Scaffold(
       body: Center(
         child: Container(
@@ -46,28 +79,181 @@ class _LoginPageState extends State<LoginPage> {
               Container(
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20),
-                  border: Border.all(width: 2),
+                  border: Border.all(
+                    width: 2,
+                    color:
+                        passwordChecked || nowPage == 0
+                            ? Colors.black
+                            : Colors.red,
+                  ),
                 ),
                 child: TextField(
+                  style: TextStyle(
+                    color:
+                        passwordChecked || nowPage == 0
+                            ? Colors.black
+                            : Colors.red,
+                  ),
+                  onChanged: (String s) {
+                    passwordChecked =
+                        s.compareTo(_passwordCheckController.text) == 0;
+                  },
                   textAlignVertical: TextAlignVertical.center,
-                  controller: _usernameController,
+                  controller: _passwordController,
+                  obscureText: _obscurePassword,
                   decoration: InputDecoration(
                     border: InputBorder.none,
                     hintText: '密码',
-                    prefixIcon: Icon(Icons.person),
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color:
+                          passwordChecked || nowPage == 0
+                              ? Colors.black
+                              : Colors.red,
+                    ),
                     suffixIcon: IconButton(
-                      icon: Icon(Icons.clear),
-                      onPressed: _usernameController.clear,
+                      icon: Icon(
+                        _obscurePassword
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                        color:
+                            passwordChecked || nowPage == 0
+                                ? Colors.black
+                                : Colors.red,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _obscurePassword = !_obscurePassword;
+                        });
+                      },
                     ),
                   ),
                 ),
               ),
+              nowPage == 1
+                  ? (Column(
+                    children: [
+                      SizedBox(height: 0.03 * height),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(
+                            width: 2,
+                            color:
+                                passwordChecked || nowPage == 0
+                                    ? Colors.black
+                                    : Colors.red,
+                          ),
+                        ),
+                        child: TextField(
+                          style: TextStyle(
+                            color:
+                                passwordChecked || nowPage == 0
+                                    ? Colors.black
+                                    : Colors.red,
+                          ),
+                          onChanged: (String s) {
+                            passwordChecked =
+                                s.compareTo(_passwordController.text) == 0;
+                          },
+                          textAlignVertical: TextAlignVertical.center,
+                          controller: _passwordCheckController,
+                          obscureText: _obscurePasswordCheck,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: '确认密码',
+                            prefixIcon: Icon(
+                              Icons.lock,
+                              color:
+                                  passwordChecked || nowPage == 0
+                                      ? Colors.black
+                                      : Colors.red,
+                            ),
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _obscurePasswordCheck
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
+                                color:
+                                    passwordChecked || nowPage == 0
+                                        ? Colors.black
+                                        : Colors.red,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _obscurePasswordCheck =
+                                      !_obscurePasswordCheck;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ))
+                  : (SizedBox()),
               SizedBox(height: 0.03 * height),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamedAndRemoveUntil(context, '/homepage', (route) => false);
-                },
-                child: Text("登录"),
+              Row(
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: login,
+                      child: Container(
+                        margin: EdgeInsets.all(0.01 * width),
+                        height: 0.05 * height,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          border: Border.all(color: Colors.blue.shade500),
+                          color:
+                              nowPage == 0
+                                  ? Colors.blue.shade500
+                                  : Colors.white,
+                        ),
+                        child: Center(
+                          child: Text(
+                            "登录",
+                            style: TextStyle(
+                              color:
+                                  nowPage == 0
+                                      ? Colors.grey.shade200
+                                      : Colors.blue.shade500,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: signUp,
+                      child: Container(
+                        margin: EdgeInsets.all(0.01 * width),
+                        height: 0.05 * height,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          border: Border.all(color: Colors.blue.shade500),
+                          color:
+                              nowPage == 1
+                                  ? Colors.blue.shade500
+                                  : Colors.white,
+                        ),
+                        child: Center(
+                          child: Text(
+                            "注册",
+                            style: TextStyle(
+                              color:
+                                  nowPage == 1
+                                      ? Colors.grey.shade200
+                                      : Colors.blue.shade500,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
