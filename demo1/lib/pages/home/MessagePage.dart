b96f1bf13451d798ed4effe15ed2cb5c64f9c9d3
张泽阳 <22373554@buaa.edu.cn>
2025-04-23 16:10:03 +0800

    zzy-MessagePage

--- demo1/lib/pages/home/MessagePage.dart
+++ demo1/lib/pages/home/MessagePage.dart
@@ -4,16 +4,203 @@ class MessagePage extends StatefulWidget {
   const MessagePage({super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    return _MessagePageState();
-  }
+  State<MessagePage> createState() => _MessagePageState();
 }
 
-class _MessagePageState extends State<MessagePage> {
+class _MessagePageState extends State<MessagePage> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final width = size.width;
+    final height = size.height;
+
     return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 0.05 * width),
+          child: Column(
+            children: [
+              SizedBox(height: 0.02 * height),
+              Container(
+                height: 0.05 * height,
+                alignment: Alignment.centerLeft,
+                child: Text("小logo", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              ),
+              Container(
+                height: 0.05 * height,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.grey[200],
+                ),
+                child: TabBar(
+                  controller: _tabController,
+                  isScrollable: false, // 平均分配宽度
+                  indicatorSize: TabBarIndicatorSize.tab, // 指示器覆盖整个 Tab
+                  indicator: BoxDecoration(
+                    color: Colors.blue[200],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  labelColor: Colors.black,
+                  unselectedLabelColor: Colors.black,
+                  tabs: const [
+                    Tab(text: "我的提问"),
+                    Tab(text: "我的回答"),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: TabBarView(
+                  controller: _tabController,
+                  children: const [
+                    MyQuestionsTab(),
+                    MyAnswersTab(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyQuestionsTab extends StatelessWidget {
+  const MyQuestionsTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final width = size.width;
+    final height = size.height;
+
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return SizedBox(
+          height: 0.15 * height,
+          child: Container(
+            margin: EdgeInsets.symmetric(vertical: 0.01 * height),
+            padding: EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.grey.shade300),
+            ),
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 0.15 * width,
+                  child: ClipOval(
+                    child: Image.asset(
+                      'assets/img.png',
+                      width: 0.15 * width,
+                      height: 0.15 * width,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                SizedBox(
+                  width: 0.65 * width,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "问题标题",
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 4),
+                      Text(
+                        "提问内容提问内容提问内容提问内容提问内容",
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class MyAnswersTab extends StatelessWidget {
+  const MyAnswersTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final width = size.width;
+    final height = size.height;
+
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return SizedBox(
+          height: 0.15 * height,
+            child: Container(
+              margin: EdgeInsets.symmetric(vertical: 0.01 * height),
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: Colors.grey.shade300),
+              ),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 0.15 * width,
+                    child: ClipOval(
+                      child: Image.asset(
+                        'assets/img.png',
+                        width: 0.15 * width,
+                        height: 0.15 * width,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  SizedBox(
+                    width: 0.65 * width,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "问题标题",
+                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          "回答内容回答内容回答内容回答内容回答内容",
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+        );
+      },
     );
   }
 }
